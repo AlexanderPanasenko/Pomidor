389409bebd199c879b3c198b02c3fc7beb400a52
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-05-26 20:44:29 +0300

    addStartTimer

--- PomidorProject/ViewController.swift
+++ PomidorProject/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, CAAnimationDelegate {
     private lazy var timeLabel: UILabel = {
         var label = UILabel()
         label.text = "25:00"
-        label.font = .systemFont(ofSize: 55)
+        label.font = .systemFont(ofSize: 40)
         label.textColor = UIColor.systemRed
         return label
     }()
@@ -66,13 +66,13 @@ class ViewController: UIViewController, CAAnimationDelegate {
         timeAndButtonStackView.translatesAutoresizingMaskIntoConstraints = false
         let horizontalConstraint = timeAndButtonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         let verticalConstraint = timeAndButtonStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        let widthConstraint = timeAndButtonStackView.widthAnchor.constraint(equalToConstant: 150)
-        let heightConstraint = timeAndButtonStackView.heightAnchor.constraint(equalToConstant:150)
+        let widthConstraint = timeAndButtonStackView.widthAnchor.constraint(equalToConstant: 170)
+        let heightConstraint = timeAndButtonStackView.heightAnchor.constraint(equalToConstant:170)
         view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
     }
     
     func drawBackLayer() {
-//        backProgressLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX, y: view.frame.midY), radius: 140, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        backProgressLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX, y: view.frame.midY), radius: 140, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
         backProgressLayer.strokeColor = UIColor.systemRed.cgColor
         backProgressLayer.fillColor = UIColor.clear.cgColor
         backProgressLayer.lineWidth = 15
@@ -80,7 +80,7 @@ class ViewController: UIViewController, CAAnimationDelegate {
     }
     
     func drawForLayer() {
-//        foreProgreessLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX, y: view.frame.midY), radius: 140, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        foreProgreessLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX, y: view.frame.midY), radius: 140, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
         foreProgreessLayer.strokeColor = UIColor.white.cgColor
         foreProgreessLayer.fillColor = UIColor.clear.cgColor
         foreProgreessLayer.lineWidth = 15
@@ -150,7 +150,7 @@ class ViewController: UIViewController, CAAnimationDelegate {
         if !isStarted{
             drawForLayer()
             startResumeAnimation()
-//            startTimer()
+            startTimer()
             isStarted = true
             startPauseButton.setImage(UIImage(systemName: "pause"), for: .normal)
         } else {
@@ -161,4 +161,47 @@ class ViewController: UIViewController, CAAnimationDelegate {
         }
     }
 
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if time < 1 && isWorkTime {
+            startPauseButton.setImage(UIImage(systemName: "play.circle"), for: .normal)
+            timer.invalidate()
+            time = 5
+            isWorkTime = false
+            isStarted = false
+            timeLabel.text = "05:00"
+            timeLabel.textColor = UIColor.systemGreen
+            startPauseButton.tintColor = UIColor.systemGreen
+            backProgressLayer.strokeColor = UIColor.systemGreen.cgColor
+        }else if time < 1 && !isWorkTime {
+            startPauseButton.setImage(UIImage(systemName: "play.circle"), for: .normal)
+            timer.invalidate()
+            time = 15
+            isWorkTime = true
+            isStarted = false
+            timeLabel.text = "25:00"
+            timeLabel.textColor = UIColor.systemRed
+            startPauseButton.tintColor = UIColor.systemRed
+            backProgressLayer.strokeColor = UIColor.systemRed.cgColor
+        } else {
+            time -= 1
+            timeLabel.text = formatTime()
+        }
+    }
+    
+    func formatTime() -> String {
+        let minutes = Int(time) / 60 % 60
+        let second = Int(time) % 60
+        return String(format: "%1d:%02d:%02d", minutes, second)
+    }
 }
+
+extension Int {
+    var degreesToRadians: CGFloat {
+        return CGFloat(self) * .pi / 180
+    }
+}
+
