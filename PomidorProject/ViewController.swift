37f54daf3c37a00e074237b90c30203bd150057e
Aleksandr Panasenko <104031583+AlexanderPanasenko@users.noreply.github.com>
2022-05-26 20:23:54 +0300

    addPrivateFunc

--- PomidorProject/ViewController.swift
+++ PomidorProject/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CAAnimationDelegate {
+    
+    private lazy var timeLabel: UILabel = {
+        var label = UILabel()
+        label.text = "25:00"
+        label.font = .systemFont(ofSize: 55)
+        label.textColor = UIColor.systemRed
+        return label
+    }()
+    
+//    private lazy var startPauseButton: UIButton = {
+//        var button = UIButton()
+//        button.setImage(UIImage(systemName: "play.circle"), for: .normal)
+//        button.tintColor = UIColor.systemRed
+//        button.addTarget(self, action: #selector(startPauseButtonAction), for: .touchUpInside)
+//        return button
+//    }()
+    
+    let foreProgreessLayer = CAShapeLayer()
+    let backProgressLayer = CAShapeLayer()
+    let animation = CABasicAnimation(keyPath: "strokeEnd")
+    var timer = Timer()
+    var isStarted = false
+    var isWorkTime = true
+    var isAnimationStarted = false
+    var time = 10
+    
+    private lazy var timeAndButtonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(stackView)
+        stackView.addArrangedSubview(timeLabel)
+//        stackView.addArrangedSubview(startPauseButton)
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setuphierarhy()
+        setupLayout()
+        setupView()
+//        drawBackLayer()
     }
-
-
+    
+    private func setuphierarhy() {
+        view.addSubview(timeLabel)
+//        view.addSubview(startPauseButton)
+    }
+    
+    private func setupView() {
+        view.backgroundColor = UIColor.black
+    }
+    
+    private func setupLayout() {
+        timeAndButtonStackView.translatesAutoresizingMaskIntoConstraints = false
+        let horizontalConstraint = timeAndButtonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let verticalConstraint = timeAndButtonStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        let widthConstraint = timeAndButtonStackView.widthAnchor.constraint(equalToConstant: 150)
+        let heightConstraint = timeAndButtonStackView.heightAnchor.constraint(equalToConstant:150)
+        view.addConstraints([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+    }
+    
 }
-
